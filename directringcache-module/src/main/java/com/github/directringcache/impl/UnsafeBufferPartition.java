87579ddaf54635aa2a743aaa5960749f7827fd07
noctarius <me@noctarius.com>
2012-10-08 14:12:38 +0200

    Added Unsafe backed PartitionBuffer implementation

---
+++ directringcache-module/src/main/java/com/github/directringcache/impl/UnsafeBufferPartition.java
@@ -0,0 +1,211 @@
+package com.github.directringcache.impl;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.github.directringcache.spi.Partition;
+import com.github.directringcache.spi.PartitionFactory;
+import com.github.directringcache.spi.PartitionSlice;
+
+public class UnsafeBufferPartition implements Partition {
+
+	public static final PartitionFactory UNSAFE_PARTITION_FACTORY = new PartitionFactory() {
+
+		@Override
+		public Partition newPartition(int sliceByteSize, int slices) {
+			return new UnsafeBufferPartition(slices, sliceByteSize);
+		}
+	};
+
+	public static class UnsafeUtil {
+
+		private static final sun.misc.Unsafe UNSAFE;
+
+		static {
+			sun.misc.Unsafe unsafe;
+			try {
+				Field unsafeField = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
+				unsafeField.setAccessible(true);
+				unsafe = (sun.misc.Unsafe) unsafeField.get(null);
+			} catch (Exception e) {
+				unsafe = null;
+			}
+
+			UNSAFE = unsafe;
+		}
+
+		private UnsafeUtil() {
+		}
+
+		public static sun.misc.Unsafe getUnsafe() {
+			return UNSAFE;
+		}
+	}
+
+	private final sun.misc.Unsafe unsafe = UnsafeUtil.getUnsafe();
+	private final long basePointer;
+	private final long allocatedLength;
+	private final UnsafePartitionSlice[] slices;
+	private final int sliceByteSize;
+	private final FixedLengthBitSet usedSlices;
+	private final AtomicInteger index = new AtomicInteger(0);
+
+	private UnsafeBufferPartition(int slices, int sliceByteSize) {
+		this.sliceByteSize = sliceByteSize;
+		this.usedSlices = new FixedLengthBitSet(slices);
+		this.slices = new UnsafePartitionSlice[slices];
+		this.allocatedLength = sliceByteSize * slices;
+		this.basePointer = unsafe.allocateMemory(allocatedLength);
+
+		for (int i = 0; i < slices; i++) {
+			this.slices[i] = new UnsafePartitionSlice(i);
+		}
+	}
+
+	@Override
+	public int available() {
+		return usedSlices.size() - usedSlices.cardinality();
+	}
+
+	@Override
+	public int used() {
+		return usedSlices.cardinality();
+	}
+
+	@Override
+	public int getSliceByteSize() {
+		return sliceByteSize;
+	}
+
+	@Override
+	public PartitionSlice get() {
+		int possibleMatch = nextSlice();
+		if (possibleMatch == -1) {
+			return null;
+		}
+
+		while (!index.compareAndSet(index.get(), possibleMatch)) {
+			possibleMatch = nextSlice();
+		}
+		synchronized (usedSlices) {
+			usedSlices.set(possibleMatch);
+		}
+		return slices[possibleMatch];
+	}
+
+	@Override
+	public void free(PartitionSlice slice) {
+		if (!(slice instanceof UnsafePartitionSlice)) {
+			throw new IllegalArgumentException("Given slice cannot be handled by this PartitionBufferPool");
+		}
+		UnsafePartitionSlice partitionSlice = (UnsafePartitionSlice) slice;
+		if (partitionSlice.getPartition() != this) {
+			throw new IllegalArgumentException("Given slice cannot be handled by this PartitionBufferPool");
+		}
+		synchronized (usedSlices) {
+			usedSlices.clear(partitionSlice.index);
+		}
+		slice.clear();
+	}
+
+	@Override
+	public void close() {
+		unsafe.setMemory(basePointer, allocatedLength, (byte) 0);
+		unsafe.freeMemory(basePointer);
+	}
+
+	private int nextSlice() {
+		if (usedSlices.isEmpty()) {
+			return -1;
+		}
+
+		int index = this.index.get();
+		int pos = index == 0 ? -1 : usedSlices.nextNotSet(index + 1);
+		if (pos == -1) {
+			pos = usedSlices.firstNotSet();
+		}
+
+		return pos;
+	}
+
+	public class UnsafePartitionSlice implements PartitionSlice {
+
+		private final int index;
+		private final int baseIndex;
+		private int writerIndex;
+		private int readerIndex;
+
+		private UnsafePartitionSlice(int index) {
+			this.index = index;
+			this.baseIndex = index * sliceByteSize;
+			clear();
+		}
+
+		@Override
+		public void clear() {
+			unsafe.setMemory(basePointer + baseIndex, sliceByteSize, (byte) 0);
+			writerIndex = 0;
+			readerIndex = 0;
+		}
+
+		@Override
+		public void put(byte value) {
+			put(writerIndex - baseIndex, value);
+		}
+
+		@Override
+		public void put(int position, byte value) {
+			unsafe.putByte(basePointer + baseIndex + position, value);
+			writerIndex++;
+		}
+
+		@Override
+		public byte read() {
+			return read(readerIndex - baseIndex);
+		}
+
+		@Override
+		public byte read(int position) {
+			readerIndex++;
+			return unsafe.getByte(basePointer + baseIndex + position);
+		}
+
+		@Override
+		public int readableBytes() {
+			return writerIndex - readerIndex;
+		}
+
+		@Override
+		public int writeableBytes() {
+			return sliceByteSize - writerIndex;
+		}
+
+		@Override
+		public int writerIndex() {
+			return writerIndex;
+		}
+
+		@Override
+		public int readerIndex() {
+			return readerIndex;
+		}
+
+		@Override
+		public void writerIndex(int writerIndex) {
+			BufferUtils.rangeCheck(writerIndex, 0, sliceByteSize, "writerIndex");
+			this.writerIndex = writerIndex;
+		}
+
+		@Override
+		public void readerIndex(int readerIndex) {
+			BufferUtils.rangeCheck(readerIndex, 0, sliceByteSize, "readerIndex");
+			this.readerIndex = readerIndex;
+		}
+
+		@Override
+		public Partition getPartition() {
+			return UnsafeBufferPartition.this;
+		}
+	}
+
+}
--- directringcache-module/src/test/java/com/github/directringcache/PartitionBufferTestCase.java
+++ directringcache-module/src/test/java/com/github/directringcache/PartitionBufferTestCase.java
@@ -2,20 +2,39 @@ package com.github.directringcache;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.github.directringcache.impl.BufferUtils;
 import com.github.directringcache.impl.ByteBufferPartition;
+import com.github.directringcache.impl.UnsafeBufferPartition;
 import com.github.directringcache.selector.RoundRobinPartitionSliceSelector;
 import com.github.directringcache.spi.PartitionFactory;
 import com.github.directringcache.spi.PartitionSliceSelector;
 
+@RunWith(Parameterized.class)
 public class PartitionBufferTestCase {
+
+	@Parameters
+	public static Collection<Object[]> parameters() {
+		return Arrays.asList(new Object[][] { { ByteBufferPartition.DIRECT_BYTEBUFFER_PARTITION_FACTORY },
+				{ ByteBufferPartition.HEAP_BYTEBUFFER_PARTITION_FACTORY }, { UnsafeBufferPartition.UNSAFE_PARTITION_FACTORY } });
+	}
+
+	private final PartitionFactory partitionFactory;
+
+	public PartitionBufferTestCase(PartitionFactory partitionFactory) {
+		this.partitionFactory = partitionFactory;
+	}
 
 	@Test
 	public void testAllocation() throws Exception {
 		PartitionSliceSelector partitionSliceSelector = new RoundRobinPartitionSliceSelector();
-		PartitionFactory partitionFactory = ByteBufferPartition.DIRECT_BYTEBUFFER_PARTITION_FACTORY;
 		PartitionBufferBuilder builder = new PartitionBufferBuilder(partitionFactory, partitionSliceSelector);
 		PartitionBufferPool pool = builder.allocatePool("500M", 50, "512K");
 
@@ -44,7 +63,6 @@ public class PartitionBufferTestCase {
 	@Test
 	public void testAllocation2() throws Exception {
 		PartitionSliceSelector partitionSliceSelector = new RoundRobinPartitionSliceSelector();
-		PartitionFactory partitionFactory = ByteBufferPartition.DIRECT_BYTEBUFFER_PARTITION_FACTORY;
 		PartitionBufferBuilder builder = new PartitionBufferBuilder(partitionFactory, partitionSliceSelector);
 		PartitionBufferPool pool = builder.allocatePool("500M", 50, "256K");
 
@@ -71,6 +89,76 @@ public class PartitionBufferTestCase {
 			pool.freePartitionBuffer(partitionBuffer);
 			System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
 
+		} finally {
+			pool.close();
+		}
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testAllocationBufferFull() throws Exception {
+		PartitionSliceSelector partitionSliceSelector = new RoundRobinPartitionSliceSelector();
+		PartitionBufferBuilder builder = new PartitionBufferBuilder(partitionFactory, partitionSliceSelector);
+		PartitionBufferPool pool = builder.allocatePool("1M", 1, "256K");
+
+		try {
+			for (int o = 0; o < 100; o++) {
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+
+				long bytes = BufferUtils.descriptorToByteSize("256K") * 20;
+
+				PartitionBuffer partitionBuffer = pool.getPartitionBuffer();
+				assertEquals(BufferUtils.descriptorToByteSize("256K"), partitionBuffer.maxCapacity());
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+
+				for (int i = 0; i < bytes + 1; i++) {
+					partitionBuffer.writeByte(1);
+				}
+				assertEquals(BufferUtils.descriptorToByteSize("256K") * 21, partitionBuffer.maxCapacity());
+				assertEquals(BufferUtils.descriptorToByteSize("256K") * 20 + 1, partitionBuffer.capacity());
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+
+				for (int i = 0; i < bytes + 1; i++) {
+					assertEquals("Wrong value at position " + i, 1, partitionBuffer.readByte());
+				}
+
+				pool.freePartitionBuffer(partitionBuffer);
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+			}
+		} finally {
+			pool.close();
+		}
+	}
+
+	@Test
+	public void testAllocationRoundRobinFullRound() throws Exception {
+		PartitionSliceSelector partitionSliceSelector = new RoundRobinPartitionSliceSelector();
+		PartitionBufferBuilder builder = new PartitionBufferBuilder(partitionFactory, partitionSliceSelector);
+		PartitionBufferPool pool = builder.allocatePool("10M", 5, "256K");
+
+		try {
+			for (int o = 0; o < 10; o++) {
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+
+				long bytes = BufferUtils.descriptorToByteSize("256K") * 20;
+
+				PartitionBuffer partitionBuffer = pool.getPartitionBuffer();
+				assertEquals(BufferUtils.descriptorToByteSize("256K"), partitionBuffer.maxCapacity());
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+
+				for (int i = 0; i < bytes + 1; i++) {
+					partitionBuffer.writeByte(1);
+				}
+				assertEquals(BufferUtils.descriptorToByteSize("256K") * 21, partitionBuffer.maxCapacity());
+				assertEquals(BufferUtils.descriptorToByteSize("256K") * 20 + 1, partitionBuffer.capacity());
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+
+				for (int i = 0; i < bytes + 1; i++) {
+					assertEquals("Wrong value at position " + i, 1, partitionBuffer.readByte());
+				}
+
+				pool.freePartitionBuffer(partitionBuffer);
+				System.out.println("Pool slices " + pool.getSliceCount() + "(" + pool.getAllocatedMemory() + " bytes), unused " + pool.getFreeSliceCount());
+			}
 		} finally {
 			pool.close();
 		}
